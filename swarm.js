652572920d4077cc147c5620039dfaf8f57238bb
soha elghany <40267477+soha-elghany@users.noreply.github.com>
2023-01-19 16:15:42 +0000

    Update swarm.js

--- swarm.js
+++ swarm.js
@@ -3,39 +3,42 @@ async function beeSwarmChart() {
     var data = await d3.json("./gdpdata.json")
     console.log(data)
 
-    var height = 1000;
-    var width = 900;
-    var margin = [50,60,50,60];
+    let height = 400;
+    let width = 900;
+    let margin = [50, 60, 50, 100];
     
-    var svg = d3.select("#bee")
+    
+    let svg = d3.select("#bee")
         .append("svg")
-        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+        .attr("width", width)
+        .attr("height", height);
 
-    var sectors = Array.from(new Set(data.map((d)=>d.Name)));
-    var xScale = d3
+    let sectors = Array.from(new Set(data.map((d) => d.Indicator)));
+    let xScale = d3
             .scaleBand()
             .domain(sectors)
-            .range([margin[3],width - margin[1]]);
+            .range([margin[3], width - margin[1]]);
 
-    var yScale = d3
+    let yScale = d3
             .scaleLinear()
-            .domain(d3.extent(data.map((d)=> d.Value)))
+            .domain(d3.extent(data.map((d) => +d["GDP____USD_billions_PPP_"])))
             .range([height - margin[2], margin[0]]);
 
-    var color = d3.scaleOrdinal().domain(sectors).range(d3.schemePaired);
+    let color = d3.scaleOrdinal().domain(sectors).range(d3.schemePaired);
 
-    svg.selectAll(".circ")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("class", "circ")
-        .attr("stroke","black")
-        .attr("fill", (d)=> color(d.Name))
-        .attr("r", (d)=> size(d.Value))
-        .attr("cx", (d)=> xScale(d.Name))
-        .attr("cy", (d)=> yScale(d.Value))
-
+    let population = d3.extent(data.map((d) => +d["population"]));
+    let size = d3.scaleSqrt().domain(population).range([3, 40]);
     
+    svg.selectAll(".circ")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "circ")
+    .attr("stroke", "black")
+    .attr("fill", (d) => color(d.Indicator))
+    .attr("r", (d) => size(d["population"]))
+    .attr("cx", (d) => xScale(d.Indicator))
+    .attr("cy", (d) => yScale(d.GDP____USD_billions_PPP_));
 
 }
 beeSwarmChart()
