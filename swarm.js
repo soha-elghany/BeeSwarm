f78d0bb8276ed551409398c2e718cd297fcf8ae1
soha elghany <40267477+soha-elghany@users.noreply.github.com>
2023-01-19 16:24:57 +0000

    Update swarm.js

--- swarm.js
+++ swarm.js
@@ -27,7 +27,7 @@ async function beeSwarmChart() {
     let color = d3.scaleOrdinal().domain(sectors).range(d3.schemePaired);
 
     let population = d3.extent(data.map((d) => +d["population"]));
-    let size = d3.scaleSqrt().domain(population).range([3, 40]);
+    let size = d3.scaleSqrt().domain(population).range([1, 10]);
     
     svg.selectAll(".circ")
     .data(data)
@@ -39,6 +39,34 @@ async function beeSwarmChart() {
     .attr("r", (d) => size(d["population"]))
     .attr("cx", (d) => xScale(d.Indicator))
     .attr("cy", (d) => yScale(d.GDP____USD_billions_PPP_));
+
+    let simulation = d3.forceSimulation(data)
+        .force("x", d3.forceX((d) => {
+            return xScale(d.Indicator);
+        }).strength(0.2))
+    
+        .force("y", d3.forceY((d) => {
+            return yScale(d.GDP____USD_billions_PPP_);
+        }).strength(1))
+    
+        .force("collide", d3.forceCollide((d) => {
+            return size(d["population"]);
+        }))
+    
+        .alphaDecay(0)
+        .alpha(0.3)
+        .on("tick", tick);
+
+    function tick() {
+            d3.selectAll(".circ")
+                .attr("cx", (d) => d.x)
+                .attr("cy", (d) => d.y);
+            }
+
+    let init_decay = setTimeout(function () {
+                console.log("start alpha decay");
+                simulation.alphaDecay(0.1);
+                }, 3000); // start decay after 3 seconds
 
 }
 beeSwarmChart()
